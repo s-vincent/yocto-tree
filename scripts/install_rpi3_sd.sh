2c152dab1dc9adac3e87c16f12bb821025fe03cb
Sebastien Vincent <sebastien.vincent@cppextrem.com>
2017-12-28 20:31:16 +0100

    Uses u-boot bootloader for Raspberry Pi 3 32-bit and 64-bit builds.

--- scripts/install_rpi3_sd.sh
+++ scripts/install_rpi3_sd.sh
@@ -21,16 +21,27 @@ fi
 
 # bootloader
 echo "Copy bootloader files..."
-cp tmp/deploy/images/raspberrypi3/bcm*dtb /media/seb/BOOT/
-cp tmp/deploy/images/raspberrypi3/bcm2835-bootfiles/* /media/seb/BOOT/
+cp tmp/deploy/images/raspberrypi3*/bcm*dtb /media/seb/BOOT/
+cp tmp/deploy/images/raspberrypi3*/bcm2835-bootfiles/* /media/seb/BOOT/
 mkdir -p /media/seb/BOOT/overlays/
-cp tmp/deploy/images/raspberrypi3/[a-z]*.dtbo /media/seb/BOOT/overlays/
-cp tmp/deploy/images/raspberrypi3/Image /media/seb/BOOT/kernel7.img
+cp tmp/deploy/images/raspberrypi3*/[a-z]*.dtbo /media/seb/BOOT/overlays/
+
+if [ ! -f tmp/deploy/images/raspberrypi3*/u-boot.bin ]; then
+  cp tmp/deploy/images/raspberrypi3*/Image /media/seb/BOOT/kernel7.img
+else
+  cp tmp/deploy/images/raspberrypi3*/*Image /media/seb/BOOT/
+  cp tmp/deploy/images/raspberrypi3*/boot.scr /media/seb/BOOT/
+
+  if [ -f tmp/deploy/images/raspberrypi3-64/Image ]; then
+    cp tmp/deploy/images/raspberrypi3*/u-boot.bin /media/seb/BOOT/kernel8.img
+  else
+    cp tmp/deploy/images/raspberrypi3*/u-boot.bin /media/seb/BOOT/kernel7.img
+  fi
+fi
 
 # image and kernel modules
 echo "Copy image and kernel modules files..."
-tar -C /media/seb/ROOT/ -xjf tmp/deploy/images/raspberrypi3/$1.tar.bz2
-#tar -C /media/seb/ROOT/ -xzf tmp/deploy/images/raspberrypi3/modules-raspberrypi3.tgz 
+tar -C /media/seb/ROOT/ -xjf tmp/deploy/images/raspberrypi3*/$1.tar.bz2
 
 echo "Syncing!"
 sync
